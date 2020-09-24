cdf06d120c9f3f4b81868d572bb94069c179ef21
“velasqueziturrate” <daniel.velasquez@correo.unimet.edu.ve>
2020-09-24 15:39:51 -0400

    ok

--- src/views/pdf/1.js
+++ src/views/pdf/1.js
@@ -10,7 +10,7 @@ export default function Ref1() {
                 <a href="https://firebasestorage.googleapis.com/v0/b/suministros-multimedica.appspot.com/o/0010-30-43040.pdf?alt=media&token=84b79255-6584-46ff-af8f-5a5f74e49934" target="_blank" rel="noopener noreferrer"><i class="fas fa-file-pdf"></i></a>
             </div>
             <div className="row justify-content-center">
-                <p>Haga clic en el ícono para abrir el PDF en otra ventana</p>
+                <p className="quote">Haga clic en el ícono para abrir el PDF en otra ventana</p>
             </div>
         </div>
     );
