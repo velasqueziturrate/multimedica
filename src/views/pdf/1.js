ca2909c00c603993a3686fb8711ba09e6e685a42
“velasqueziturrate” <daniel.velasquez@correo.unimet.edu.ve>
2020-09-24 15:20:26 -0400

    PDF

--- src/views/pdf/1.js
+++ src/views/pdf/1.js
@@ -4,10 +4,13 @@ export default function Ref1() {
     return (
         <div>
             <div className="row presentation justify-content-center text-center">
-                <h1 className="text-center">BASE DE PARED PARA MONITOR BENEVIEW T5</h1>
+                <h1 className="text-center title">BASE DE PARED PARA MONITOR BENEVIEW T5</h1>
             </div>
             <div className="row justify-content-center">
-                <embed src="https://firebasestorage.googleapis.com/v0/b/suministros-multimedica.appspot.com/o/0010-30-43040.pdf?alt=media&token=84b79255-6584-46ff-af8f-5a5f74e49934" type="application/pdf" className="pdf" />
+                <a href="https://firebasestorage.googleapis.com/v0/b/suministros-multimedica.appspot.com/o/0010-30-43040.pdf?alt=media&token=84b79255-6584-46ff-af8f-5a5f74e49934" target="_blank" rel="noopener noreferrer"><i class="fas fa-file-pdf"></i></a>
+            </div>
+            <div className="row justify-content-center">
+                <p>Haga clic en el ícono para abrir el PDF en otra ventana</p>
             </div>
         </div>
     );
